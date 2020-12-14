285f3b9baa3b636faa3b3471de0c838c8bb7bfc8
geekon <kacperpiasta@gmail.com>
2020-12-14 23:15:17 +0100

    1.3. Praca na gałęziach -> punkt 1 i 2: błędy formatowania

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,19 +3,20 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends defaultCountingOutRhymer {
 
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
+
 	@Override
 	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
+		while (!callCheck()) {
+			temp.countIn(super.countOut());
+		}
+
 		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
+
+		while (!temp.callCheck()) {
+			countIn(temp.countOut());
+		}
+
 		return ret;
 	}
+
 }
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -6,9 +6,9 @@ public class IntLinkedList {
 	int i;
 
 	public void push(int i) {
-		if (last == null)
+		if (last == null) {
 			last = new Node(i);
-		else {
+		} else {
 			last.next = new Node(i);
 			last.next.prev = last;
 			last = last.next;
@@ -24,14 +24,16 @@ public class IntLinkedList {
 	}
 
 	public int top() {
-		if (isEmpty())
+		if (isEmpty()) {
 			return -1;
+		}
 		return last.value;
 	}
 
 	public int pop() {
-		if (isEmpty())
+		if (isEmpty()) {
 			return -1;
+		}
 		int ret = last.value;
 		last = last.prev;
 		return ret;
