4c5ab471fde2a92c83df0b7925b8c1201d1e3a28
geekon <kacperpiasta@gmail.com>
2020-12-14 23:49:18 +0100

    1.3. Praca na gałęziach -> punkt 4 i 5: class-method-naming -> błędy konwencji klas

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,15 +1,15 @@
 package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
-import edu.kis.vh.nursery.factory.Rhymersfactory;
+import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
 	public static void main(String[] args) {
 
-		Rhymersfactory factory = new DefaultRhymersFactory();
+		RhymersFactory factory = new DefaultRhymersFactory();
 
-		defaultCountingOutRhymer[] rhymers = {
+		DefaultCountingOutRhymer[] rhymers = {
 				factory.getStandardRhymer(),
 				factory.getFalseRhymer(),
 				factory.getFIFORhymer(),
