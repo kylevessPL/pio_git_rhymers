29fde81d8e5da8a3586954d2c19d9ccceca03f59
geekon <kacperpiasta@gmail.com>
2021-01-13 15:28:15 +0100

    wygenerowanie szkieletu dokumentacji do klasy HanoiRhymer i jej metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,26 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The type Hanoi Rhymer
+ * extends type of DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	private int totalRejected = 0;
 
+	/**
+	 * Report number of rejected
+	 *
+	 * @return the int
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * count method
+	 * overrides DefaultCountingOutRhymer method
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo()) {
